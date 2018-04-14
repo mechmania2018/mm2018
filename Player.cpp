97c26fb0db904f551d6e007d4adeebd5f3cd357e
NoahFeinberg <noahbfeinberg@gmail.com>
2018-04-14 15:06:14 -0500

    Update Player.cpp
    removed std form json script.

--- Player.cpp
+++ Player.cpp
@@ -65,14 +65,14 @@ string Player::get_string() {
     return "Name: " + get_name() + ", health = " + to_string(get_health()) + ", kung fu = " + to_string(get_kung_fu()) + ", speed = " + to_string(get_speed()) + ", exp = " + to_string(_exp_points);
 }
 
-std::string Player::to_json() {
-    std::string as_json = "{";
+string Player::to_json() {
+    string as_json = "{";
     as_json = as_json + "\"name\": " + "\"" + _name + "\",";
-    as_json = as_json + "\"health\": " + "\"" + std::to_string(_health)+ "\",";
-    as_json = as_json + "\"kung_fu\": " + "\"" + std::to_string(_kung_fu)+ "\",";
-    as_json = as_json + "\"speed\": " + "\"" + std::to_string(_speed)+ "\",";
-    as_json = as_json + "\"movement_counter\": " + "\"" + std::to_string(_movement_counter)+ "\",";
-    as_json = as_json + "\"location\": " + "\"" + std::to_string(_location)+ "\",";
-    as_json = as_json + "\"destination\": " + "\"" + std::to_string(_destination)+ "\"";
+    as_json = as_json + "\"health\": " + "\"" + to_string(_health)+ "\",";
+    as_json = as_json + "\"kung_fu\": " + "\"" + to_string(_kung_fu)+ "\",";
+    as_json = as_json + "\"speed\": " + "\"" + to_string(_speed)+ "\",";
+    as_json = as_json + "\"movement_counter\": " + "\"" + to_string(_movement_counter)+ "\",";
+    as_json = as_json + "\"location\": " + "\"" + to_string(_location)+ "\",";
+    as_json = as_json + "\"destination\": " + "\"" + to_string(_destination)+ "\"";
     as_json = as_json + "}";
 }
